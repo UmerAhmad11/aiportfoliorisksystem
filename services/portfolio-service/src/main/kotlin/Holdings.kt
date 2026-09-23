8354832f11f927f1adb36b05ebbcac5e3d9cdb33
Umer Ahmad <sumerahmad2002@gmail.com>
2026-09-24 02:33:56 +0300

    Create Holdings Data Class

---
+++ services/portfolio-service/src/main/kotlin/Holdings.kt
@@ -0,0 +1,7 @@
+data class Holdings(
+    val id: String,
+    val assetSymbol: String,       // e.g., "AAPL", "BTC"
+    val quantity: Double,          // e.g., 10.5
+    val averageBuyPrice: Double,   // e.g., 175.50
+    val assetType: String          // e.g., "STOCK", "CRYPTO"
+)
